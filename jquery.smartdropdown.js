afc60e54aa1c1ab94dc6b003abd4e732534c8395
Evgeny E. Neverov <evgeny@neverov.name>
2015-02-27 15:37:37 +0700

    много изменений

--- jquery.smartdropdown.js
+++ jquery.smartdropdown.js
@@ -1,11 +1,28 @@
 (function($) {
-  /**
+	/**
 	 * smartDropdown plugin creates a customizable dropdown from <input> tag
-	 * 
+	 * © github.com/deMoneIrk/jQuery.smartDropdown
+	 *
 	 * TODO: автоматический ресайз поля для ввода e-mail
+	 * TODO: анализ текстового поля при вставке готовой строки со списком e-mail адресов
+	 * TODO: не разрешать вводить несколько одинаковых e-mail адресов
 	 * */
 	$.fn.smartDropdown = function(options) {
 		var methods = {
+			// Уничтожаем smartDropdown
+			destroy: function() {
+				methods.hide.apply(this, []);
+
+				$(this).off('.smartDropdown');
+				if ($(this).data('smartDropdown'))
+					$(this).data('smartDropdown').config.snapTo.off('.smartDropdown');
+
+				$(this).data('smartDropdown').dropdown.remove();
+				$(this).removeData('smartDropdown');
+
+				return true;
+			},
+
 			// Скрывает поле при потере фокуса элементов
 			blur: function() {
 				var _this = this, $this = $(this),
@@ -17,29 +34,54 @@
 					ok = false;
 
 				for (var i in data.list) {
-					if ((data.list[i].name ? data.list[i].name : data.list[i].title).trim() == value) {
+					if (i == 'length') continue;
+
+					if ((data.list[i].name ? data.list[i].name : data.list[i].title).trim().toLowerCase() == value.trim().toLowerCase()) {
 						ok = true;
 						$(data.config.idField).val(data.list[i].id);
+
+						data.list[i].yetSelected = true;
+						data.list[i].selectedInField = data.id;
+
+						if (typeof data.config.onItemSelect == 'function')
+							data.config.onItemSelect.apply(this, [data.list[i]]);
+
+						break;
 					}
 				}
 
 				if (!ok) {
+					// Если в текущем поле никакой элемент не выбран, то убираем yetSelected у предыдущего
+					// заданного варианта
+					if (!data.config.multiple) {
+						for (var i in data.list)
+							if (i !== 'length' && data.list[i].selectedInField == data.id)
+								data.list[i].yetSelected = false;
+					}
+
 					// Если текст не найден, и новые элементы создавать не разрешается, то поле нужно обнулить
 					if (!data.config.allowNewItems) {
 						$(this).val('');
 					} else {
 						if (data.config.multiple) {
 							if (typeof data.config.newItemCheck != 'function' || data.config.newItemCheck(value)) {
-								var html = $('<div class="selected-item"><input type="hidden" name="item[_new][]" value="' + value + '" />' +
-									'<span>' + value + '</span> <i></i></div>');
+								var html = $('<div class="selected-item"><input type="hidden" name="' + data.config.fieldName + '[_new][]" value="' + value +
+									'" /><span>' + value + '</span> <i></i></div>');
 
-								html.find('i').click(function() {
+								html.find('i').on('click.smartDropdown', function() {
+									$(this).closest('.selected-item').data('item') && ($(this).closest('.selected-item').data('item').yetSelected = false);
 									$(this).closest('.selected-item').fadeOut(100, function() { $(this).remove(); });
 								});
 
 								$this.before(html);
+
+								if (typeof data.config.onEnterNewValue == 'function')
+									data.config.onEnterNewValue.apply(this, [value]);
 							}
 							$this.val('');
+						} else {
+							if (typeof data.config.onEnterNewValue == 'function')
+								data.config.onEnterNewValue.apply(this, [$(this).val()]);
 						}
 					}
 
@@ -57,17 +99,19 @@
 				var $this = $(this),
 					data = $this.data('smartDropdown');
 
-				if (data.list && data.list.length > 0) {
+				if (data.list && data.list.length) {
 					var ul = data.dropdown.find('ul');
 					ul.html('');
 					for (var i in data.list) {
-						data.list[i]._item = $('<li>' + data.config.itemOutput(data.list[i]) + '</li>');
-						data.list[i]._item.data({
+						if (i == 'length') continue;
+
+						data.itemList[i] = $('<li>' + data.config.itemOutput(data.list[i]) + '</li>');
+						data.itemList[i].data({
 							item: data.list[i],
 							index: i
-						}).bind('click.smartDropdown', function(e) {
+						}).on('click.smartDropdown', function(e) {
 							methods.itemClick.apply(this, [e, $this]);
-						}).mouseenter(function() {
+						}).on('mouseenter.smartDropdown', function() {
 							$(this).closest('ul').find('.hover').removeClass('hover');
 							$(this).addClass('hover');
 
@@ -75,14 +119,38 @@
 						}).appendTo(ul);
 					}
 				}
+
+				data.dropdown.find('.borders').mCustomScrollbar({
+					advanced: {
+						updateOnBrowserResize: true,
+						updateOnContentResize: true
+					},
+					autoHideScrollbar: false,
+					contentTouchScroll: true,
+					scrollButtons: {
+						enable: false
+					},
+					scrollInertia: 100,
+					theme: 'dark'
+				});
 			},
 
 			// Отлавливает события клавиатуры и обрабатывает их, кроме того применяет фильтрацию к элементам
 			catchKey: function(e) {
 				var data = $(this).data('smartDropdown');
 
-				if (e.which == 9) {
+				if (e.which == 9) { // уход из поля по Tab-у
 					methods.hide.apply(this);
+				}
+
+				// В случае, когда возможен ввод множества значений, обрываем ввод по запятой и точке с запятой
+				if (data.config.multiple && this.value && this.value.search(/[;,]/) > -1) {
+					e.preventDefault();
+					if (this.value.match(/[;,\s]$/))
+						this.value = this.value.substr(0, this.value.length - 1);
+
+					methods.blur.apply(this);
+					return false;
 				}
 
 				if (e.which in {38: 'down', 40: 'up', 13: 'enter', 27: 'esc'}) {
@@ -99,7 +167,10 @@
 						data.dropdown.find('.hover').removeClass('hover');
 
 						data.currentHoveredItem = next.prevAll('li').length;
-						data.list[data.currentHoveredItem]._item.addClass('hover');
+						data.itemList[data.currentHoveredItem].addClass('hover');
+
+						if (typeof data.config.onItemKeyboardHover == 'function')
+							data.config.onItemKeyboardHover.apply(this, [data.itemList[data.currentHoveredItem]]);
 					} else if (e.which == 38 && data.currentHoveredItem > 0) {
 						var prev = data.dropdown.find('.hover').prevAll('li:visible:first');
 						if (!prev.length) return false;
@@ -107,16 +178,21 @@
 						data.dropdown.find('.hover').removeClass('hover');
 
 						data.currentHoveredItem = prev.prevAll('li').length;
-						data.list[data.currentHoveredItem]._item.addClass('hover');
+						data.itemList[data.currentHoveredItem].addClass('hover');
+
+						if (typeof data.config.onItemKeyboardHover == 'function')
+							data.config.onItemKeyboardHover.apply(this, [data.itemList[data.currentHoveredItem]]);
 					} else if (e.which == 27) {
 						data.hiddenByUser = true;
 						methods.hide.apply(this);
 					} else if (e.which == 13) {
 						if (data.currentHoveredItem > -1) {
+							// Если есть элемент в фокусе, выбираем его
 							data.dropdown.find('.hover a').click();
 						} else if (data.dropdown.find('li:visible').length == 1) {
+							// Если элемент единственный показываемый, выбираем его
 							data.dropdown.find('li:visible').click();
-						} else if (data.config.allowNewItems) {
+						} else if (data.config.multiple && data.config.allowNewItems) {
 							var $this = $(this),
 								value = $this.val();
 
@@ -126,10 +202,11 @@
 								return false;
 							}
 
-							var html = $('<div class="selected-item"><input type="hidden" name="item[_new][]" value="' + value + '" />' +
-								'<span>' + value + '</span> <i></i></div>');
+							var html = $('<div class="selected-item"><input type="hidden" name="' + data.config.fieldName + '[_new][]" value="' + value +
+								'" /><span>' + value + '</span> <i></i></div>');
 
-							html.find('i').click(function() {
+							html.find('i').on('click.smartDropdown', function() {
+								$(this).closest('.selected-item').data('item') && ($(this).closest('.selected-item').data('item').yetSelected = false);
 								$(this).closest('.selected-item').fadeOut(100, function() { $(this).remove(); });
 							});
 
@@ -138,22 +215,37 @@
 
 							methods.setPosition.apply($this);
 							methods.catchKey.apply($this, [{ which: -1 }]);
-						}
 
-						return false;
+							if (typeof data.config.onEnterNewValue == 'function')
+								data.config.onEnterNewValue.apply(this, [value]);
+						}
 					}
+
+					if (typeof data.config.onCatchKey == 'function')
+						data.config.onCatchKey.apply(this);
 
 					return false;
 				}
 
 				// Item list filter
+				var showCount = 0;
 				for (var i in data.list)
-					data.list[i]._item[data.config.filterCallback(data.list[i], this.value, data.config) ? 'show' : 'hide']();
+					if (i != 'length') {
+						var action = data.config.filterCallback(data.list[i], this.value, data.config) ? 'show' : 'hide';
+						if (action == 'show') showCount++;
 
-				methods.setVisibility.apply(this);
+						data.itemList[i][action]();
+					}
+
+				data.dropdown.css('visibility', showCount ? 'visible' : 'hidden').find('.borders').css('height', Math.min(showCount * 41 - 1, data.config.maxHeight)).mCustomScrollbar('update');
+
+				data.config.snapTo[showCount ? 'removeClass' : 'addClass']('sdd-no-items');
 
 				if (!data.hiddenByUser)
 					methods.show.apply(this);
+
+				if (typeof data.config.onCatchKey == 'function')
+					data.config.onCatchKey.apply(this);
 
 				return true;
 			},
@@ -178,10 +270,12 @@
 					// Класс, добавляемый к выпадающему списку
 					dropdownClass: '',
 
+					fieldName: 'item',
+
 					// Вызывается при фильтрации элементов по полям объекта
 					filterCallback: function(item, query, config) {
-						// Если элемент уже выбран в мультиселекте, то его больше не выводим
-						if (config.multiple && item.yetSelected)
+						// Если элемент уже выбран, то его больше не выводим
+						if (item.yetSelected)
 							return false;
 
 						if (!query) return true;
@@ -218,25 +312,41 @@
 					// Используется для задания отступа выпадающего меню от <input>-а
 					marginTop: 0,
 
+					maxHeight: 204,
+
 					// Разрешать выбирать сразу несколько элементов
 					multiple: false,
 
 					// Функция проверки корректности нового добавляемого элемента
 					newItemCheck: function(text) { return true; },
 
+					// Callback срабатывает на каждый catchKey, кроме системных клавиш
+					onCatchKey: function() { return true; },
+
+					// Callback срабатывает при вводе пользователем собственного значения
+					onEnterNewValue: function(val) { return true; },
+
+					// Callback срабатывает при переходе на item по клавиатуре
+					onItemKeyboardHover: function(hoveredItem) { return true; },
+
+					// Callback срабатывает после инициализации компонента
+					onLoad: function() { return true; },
+
+					// Callback срабатывает при выборе пункта выпадающего списка
+					onItemSelect: function(item) { return true; },
+
 					// Поля исходного объекта, по которым можно производить поиск при вводе текста
 					searchFields: ['name', 'title'],
 
 					// Можно указать, к какому конкретно элементу привязывать выпадающий список
 					snapTo: false,
-					snappingToOtherObject: false, // флаг того, что мы привязываемся к чужому объекту
-
-					// Количество выводимых на экран элементов
-					visibleCount: 5
+					snappingToOtherObject: false // флаг того, что мы привязываемся к чужому объекту
 				}, config);
 
 				var $this = $(this),
 					data = $this.data('smartDropdown');
+
+				$this.attr('autocomplete', 'off');
 
 				if (!config.snapTo)
 					config.snapTo = $this;
@@ -247,30 +357,60 @@
 					return true;
 
 				for (var i in config.list) {
+					if (i == 'length') continue;
+
 					if (typeof config.list[i] == 'string')
 						config.list[i] = {name: config.list[i]};
 
-					config.list[i].yetSelected = false;
+					if (config.list[i].yetSelected == undefined) {
+						config.list[i].yetSelected = false;
+						config.list[i].selectedInField = null;
+					}
 				}
 
 				data = {
 					id: 'sdd' + Math.random(),
 					config: config,
 					currentHoveredItem: -1,
-					dropdown: $('<div class="smart-dropdown' + (config.dropdownClass ? ' ' + config.dropdownClass : '') + '"><ul></ul></div>'),
+					dropdown: $('<div class="smart-dropdown' + (config.dropdownClass ? ' ' + config.dropdownClass : '') + '"><div class="borders"><ul></ul></div></div>'),
 					hiddenByUser: false,
-					list: config.list
+					itemList: {}
 				};
+
+				data.list = config.list;
+
+				if (config.multiple) {
+					if ($(data.config.snapTo).find('.selected-item').length) {
+						for(var i in data.list) {
+							var selectedItem = $(data.config.snapTo).find('.selected-item input[type=hidden][name*="' + data.list[i].id + '"]');
+							if (selectedItem.length) {
+								selectedItem.closest('.selected-item').addClass('selected-item-linked').data('item', data.list[i]).find('i').on('click', function() {
+									$(this).closest('.selected-item').data('item') && ($(this).closest('.selected-item').data('item').yetSelected = false);
+									$(this).closest('.selected-item').fadeOut(100, function() { $(this).remove(); });
+								});
+
+								data.list[i].yetSelected = true;
+								data.list[i].selectedInField = data.id;
+							}
+						}
+					}
+
+					var notLinked = $(data.config.snapTo).find('.selected-item:not(.selected-item-linked)');
+					if (notLinked.length)
+						for(var i = 0; i < notLinked.length; i++)
+							notLinked.eq(i).find('i').on('click', function() {
+								$(this).closest('.selected-item').fadeOut(100, function() { $(this).remove(); });
+							});
+				}
 
 				$this.data('id', data.id);
 				data.dropdown.data('input', $this);
 
 				$this.data('smartDropdown', data);
 				methods.build.apply(this);
-				methods.setVisibility.apply(this);
 
 				if (config.snappingToOtherObject)
-					config.snapTo.bind('click.smartDropdown', function(e) {
+					config.snapTo.on('click.smartDropdown', function(e) {
 						if ($(e.target).hasClass('selected-item') || $(e.target).closest('.selected-item').length)
 							return false;
 
@@ -281,9 +421,9 @@
 					});
 
 				$this
-					.bind('focus.smartDropdown', methods['show'])
-					.bind('keyup.smartDropdown', methods['catchKey'])
-					.bind('keydown.smartDropdown', function(e) {
+					.on('focus.smartDropdown', methods['show'])
+					.on('keyup.smartDropdown', methods['catchKey'])
+					.on('keydown.smartDropdown', function(e) {
 						/**
 						 * Отслеживаем backspace для удаления уже выбранных приглашаемых
 						 * */
@@ -326,13 +466,23 @@
 								i.removeClass('prepared-for-delete');
 						}
 					})
-					.bind('keypress.smartDropdown', function(e) {
+					.on('keypress.smartDropdown', function(e) {
 						if (e.which == 13) {
 							e.preventDefault();
 							return false;
 						}
 					})
-					.bind('blur.smartDropdown', methods['blur']);
+					.on('blur.smartDropdown', methods['blur']);
+
+				$(window).resize(function() {
+					if (!$this.data('smartDropdown').dropdownVisible)
+						return false;
+
+					methods.setPosition.apply($this);
+				});
+
+				if (typeof config.onLoad == 'function')
+					config.onLoad.apply(this);
 			},
 
 			// Отрабатывает при клике на элемент выпадающего списка
@@ -341,7 +491,15 @@
 					item = $(this).data('item');
 
 				if (!data.config.multiple) {
-					input.val(item.name ? item.name : item.title);
+					input.val(item.name ? item.name : item.title).change();
+
+					for (var i in data.list)
+						if (i !== 'length' && data.list[i].selectedInField == data.id)
+							data.list[i].yetSelected = false;
+
+					item.yetSelected = true;
+					item.selectedInField = data.id;
+
 					input.focus();
 
 					methods.hide.apply(input);
@@ -350,11 +508,11 @@
 					if (idField.length)
 						idField.val(item.id);
 				} else {
-					var html = $('<div class="selected-item"><input type="hidden" name="item[' + item.id + ']" value="' + (item.name ? item.name : item.title) + '" />' +
-						'<span>' + (item.name ? item.name : item.title) + '</span> <i></i></div>').data('item', item);
+					var html = $('<div class="selected-item"><input type="hidden" name="' + data.config.fieldName + '[' + item.id + ']" value="' +
+						(item.name ? item.name : item.title) + '" /><span>' + (item.name ? item.name : item.title) + '</span> <i></i></div>').data('item', item);
 
-					html.find('i').click(function() {
-						$(this).closest('.selected-item').data('item').yetSelected = false;
+					html.find('i').on('click', function() {
+						$(this).closest('.selected-item').data('item') && ($(this).closest('.selected-item').data('item').yetSelected = false);
 						$(this).closest('.selected-item').fadeOut(100, function() {
 							$(this).remove();
 						});
@@ -363,6 +521,8 @@
 					input.before(html);
 
 					item.yetSelected = true;
+					item.selectedInField = data.id;
+
 					data.dropdown.find('.hover').removeClass('hover');
 					data.currentHoveredItem = -1;
 					input.val('');
@@ -370,6 +530,10 @@
 					methods.setPosition.apply(input);
 					methods.catchKey.apply(input, [{ which: -1 }]);
 				}
+
+				// Запускаем callback
+				if (typeof data.config.onItemSelect == 'function')
+					data.config.onItemSelect.apply(input, [item]);
 			},
 
 			// Позиционирует выпадающий список
@@ -378,28 +542,11 @@
 					data = $this.data('smartDropdown'),
 					dd = data.dropdown;
 
-				// console.log('setPosition', data.config.snapTo.position().top, data.config.snapTo.outerHeight());
-
 				dd.css({
-					left: data.config.snapTo.position().left,
-					top: data.config.snapTo.position().top + data.config.snapTo.outerHeight() + data.config.marginTop,
+					left: data.config.snapTo.offset().left,
+					top: data.config.snapTo.offset().top + data.config.snapTo.outerHeight() + data.config.marginTop, // был position().top
 					width: data.config.snapTo.outerWidth()
 				});
-			},
-
-			// Скрывает те элементы списка, которых больше, чем config.visibilityCount
-			setVisibility: function() {
-				var $this = $(this),
-					data = $this.data('smartDropdown'),
-					vCount = 0;
-
-				for (var i in data.list) {
-					if (data.list[i]._item.css('display') != 'none') {
-						vCount++;
-						if (vCount > data.config.visibleCount)
-							data.list[i]._item.hide();
-					}
-				}
 			},
 
 			// Отображает выпадающий список по событиям
@@ -409,8 +556,20 @@
 
 				if (data.dropdownVisible) return false;
 
+				// Item list filter
+				var showCount = 0;
+				for (var i in data.list)
+					if (i != 'length') {
+						var action = data.config.filterCallback(data.list[i], this.value, data.config) ? 'show' : 'hide';
+						if (action == 'show') showCount++;
+
+						data.itemList[i][action]();
+					}
+
 				data.dropdownVisible = true;
-				data.dropdown.appendTo('body');
+				data.dropdown.appendTo('body').css('visibility', showCount ? 'visible' : 'hidden').find('.borders').css('height', Math.min(showCount * 41 - 1, data.config.maxHeight)).mCustomScrollbar('update');
+
+				data.config.snapTo[showCount ? 'removeClass' : 'addClass']('sdd-no-items');
 
 				data.dropdown.find('.hover').removeClass('hover');
 				data.currentHoveredItem = -1;
@@ -426,6 +585,8 @@
 		var args = arguments;
 
 		return this.each(function() {
+			if ((args[0] && args[0] == 'destroy') && !$(this).data('smartDropdown')) return true;
+
 			if (!$(this).data('smartDropdown')) {
 				methods['init'].apply(this, args);
 			} else if (methods[options]) {
